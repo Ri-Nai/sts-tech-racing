fb872a4612fc73c72197138832f69ef18d27d306
TNoName <604689206@qq.com>
2023-10-04 05:26:48 +0800

    尝试对商店后问号SL和事件战斗后SL导致的房间变化进行修复

--- src/main/java/demoMod/cfcracing/CatFoodCupRacingMod.java
+++ src/main/java/demoMod/cfcracing/CatFoodCupRacingMod.java
@@ -23,6 +23,8 @@ import com.megacrit.cardcrawl.ui.DialogWord;
 import com.megacrit.cardcrawl.vfx.SpeechTextEffect;
 import demoMod.cfcracing.entity.CardSetting;
 import demoMod.cfcracing.patches.SaveLoadCheck;
+import demoMod.cfcracing.patches.rngfix.EventHelperPatch;
+import demoMod.cfcracing.patches.rngfix.MonsterHelperPatch;
 import demoMod.cfcracing.ui.CardFilterModMenu;
 
 import java.io.IOException;
@@ -72,6 +74,9 @@ public class CatFoodCupRacingMod implements StartGameSubscriber,
             saves.setInt("matchGameLastFloor", -1);
             saves.setInt("merchantRngLastFloor", -1);
             saves.setFloat("totalTime", 0);
+
+            saves.setInt("EventRngCountLast", -1);
+            saves.setString("EventResultLast", "");
             try {
                 saves.save();
             } catch (IOException e) {
@@ -206,6 +211,7 @@ public class CatFoodCupRacingMod implements StartGameSubscriber,
         Texture badgeTexture = ImageMaster.loadImage("cfcImages/ui/badge.png");
         BaseMod.registerModBadge(badgeTexture, "CFC Racing Mod", "Temple9", "todo", settingsPanel);
         BaseMod.addSaveField("cfc:SLCheck", new SaveLoadCheck());
+        BaseMod.addSaveField("cfc:encounterPool", new MonsterHelperPatch());
         if (saves.has("defaultA15Option")) {
             defaultA15Option = saves.getBool("defaultA15Option");
         }
